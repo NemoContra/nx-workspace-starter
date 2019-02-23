5dc5e53c6a034f99bbcca9112c855040ea7ab3a2
Marco <m.haemmerle@nemocontra.de>
2019-02-23 13:56:36 +0100

    final exception filter labs

--- apps/flight-api/src/app/app.module.ts
+++ apps/flight-api/src/app/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FlightModule } from './flight/flight.module';
-import { PassengerModule } from './passenger/passenger.module';
 
 @Module({
-  imports: [FlightModule, PassengerModule],
+  imports: [FlightModule],
   controllers: [AppController],
   providers: [AppService],
 })
