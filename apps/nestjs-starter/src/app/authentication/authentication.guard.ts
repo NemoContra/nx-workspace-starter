853e6d267fbd565bb8f53b866b6bee24a8e049af
Marco <m.haemmerle@nemocontra.de>
2019-02-23 13:56:36 +0100

    final middleware labs

---
+++ apps/nestjs-starter/src/app/authentication/authentication.guard.ts
@@ -0,0 +1,26 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class AuthenticationGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest();
+    const authorizationHeader = request.headers['authorization'];
+    if (
+      authorizationHeader &&
+      authorizationHeader.indexOf('Bearer jwt123456token') !== -1 &&
+      request.user &&
+      request.user.role === 'admin'
+    ) {
+      return true;
+    }
+    throw new UnauthorizedException();
+  }
+}
