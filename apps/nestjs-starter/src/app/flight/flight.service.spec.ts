5836b6925bdb29a89fa64b2d19aa8d85214436b2
Marco <m.haemmerle@nemocontra.de>
2019-02-23 13:56:36 +0100

    final http client labs

---
+++ apps/nestjs-starter/src/app/flight/flight.service.spec.ts
@@ -0,0 +1,120 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { FlightService } from './flight.service';
+import { of, throwError } from 'rxjs';
+import { Flight } from '@flight-app/shared';
+import { HttpModule, HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
+
+describe('FlightService', () => {
+  let service: FlightService;
+  let httpService: HttpService;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      imports: [HttpModule],
+      providers: [FlightService],
+    }).compile();
+    service = module.get<FlightService>(FlightService);
+    httpService = module.get<HttpService>(HttpService);
+  });
+
+  it('should return an empty array if nothing is passed in "searchFlights"', () => {
+    expect(service.getFlights('', '')).toEqual([]);
+  });
+
+  it('should return correct flights if "Stuttgart" and "Salzburg" is passed in "searchFlights"', () => {
+    const result = service.getFlights(
+      'Stuttgart',
+      'Salzburg',
+      new Date('2019-02-28T00:06:54'),
+      new Date('2019-03-02T00:08:54')
+    );
+    expect(result.length).toEqual(3);
+    expect(result).toEqual([
+      {
+        id: 171,
+        from: 'Stuttgart',
+        to: 'Salzburg',
+        date: '2019-03-01T09:07:54.1624336+00:00',
+        delayed: false,
+      },
+      {
+        id: 172,
+        from: 'Stuttgart',
+        to: 'Salzburg',
+        date: '2019-03-01T10:07:54.1624336+00:00',
+        delayed: false,
+      },
+      {
+        id: 173,
+        from: 'Stuttgart',
+        to: 'Salzburg',
+        date: '2019-03-01T11:07:54.1624336+00:00',
+        delayed: false,
+      },
+    ]);
+  });
+
+  it('should return correct flight for "getFlightById"', () => {
+    const mockData: Flight = {
+      id: 4,
+      from: 'Hamburg',
+      to: 'Graz',
+      date: '2019-02-22T09:07:54.1624336+00:00',
+      delayed: false,
+    };
+
+    jest
+      .spyOn(httpService, 'get')
+      .mockReturnValue(of({ data: mockData } as AxiosResponse<Flight>));
+
+    service
+      .getFlightById(4)
+      .subscribe((data) => expect(data).toEqual(mockData));
+    expect(httpService.get).toHaveBeenCalledWith(
+      'http://www.angular.at/api/flight/4'
+    );
+  });
+
+  it('should handle a HTTP-Error correctly for "getFlightById"', (done) => {
+    jest
+      .spyOn(httpService, 'get')
+      .mockReturnValue(throwError(() => ({ response: { status: 400 } })));
+
+    service.getFlightById(4).subscribe({
+      next: () => fail,
+      error: (error) => {
+        expect(error.status).toEqual(400);
+        done();
+      },
+    });
+    expect(httpService.get).toHaveBeenCalledWith(
+      'http://www.angular.at/api/flight/4'
+    );
+  });
+
+  it('should return correct flight for "createFlight"', () => {
+    expect(
+      service.createFlight({
+        from: 'Hamburg',
+        to: 'Graz',
+        date: '2019-02-22T09:07:54.1624336+00:00',
+        delayed: false,
+      })
+    ).toEqual({
+      id: 174,
+      from: 'Hamburg',
+      to: 'Graz',
+      date: '2019-02-22T09:07:54.1624336+00:00',
+      delayed: false,
+    });
+  });
+
+  it('should return true for "deleteFlight"', () => {
+    expect(service.deleteFlight(4)).toEqual(true);
+  });
+
+  it('should return false for "deleteFlight"', () => {
+    expect(service.deleteFlight(190)).toEqual(false);
+  });
+});
---
+++ apps/nestjs-starter/src/app/flight/flight.service.ts
@@ -0,0 +1,62 @@
+import { HttpException, Injectable } from '@nestjs/common';
+import { HttpService } from '@nestjs/axios';
+import { Flight } from '@flight-app/shared';
+import flights from '../../../mock-data/flights.json';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import { AxiosError } from 'axios';
+
+@Injectable()
+export class FlightService {
+  private flights: Flight[] = flights;
+
+  constructor(private httpService: HttpService) {}
+
+  public getFlights(
+    from: string,
+    to: string,
+    fromDate?: Date,
+    toDate?: Date
+  ): Flight[] {
+    let filteredFlights = this.flights;
+    if (fromDate) {
+      filteredFlights = filteredFlights.filter(
+        (flight) => new Date(flight.date) >= fromDate
+      );
+    }
+    if (toDate) {
+      filteredFlights = filteredFlights.filter(
+        (flight) => new Date(flight.date) <= toDate
+      );
+    }
+    return filteredFlights.filter(
+      (flight) => flight.from === from && flight.to === to
+    );
+  }
+
+  public getFlightById(id: number): Observable<Flight> {
+    return this.httpService
+      .get<Flight>(`http://www.angular.at/api/flight/${id}`)
+      .pipe(
+        map((res) => res.data),
+        catchError((error: AxiosError) =>
+          throwError(() => new HttpException('', error.response.status))
+        )
+      );
+  }
+
+  public createFlight(flight: Flight): Flight {
+    const newFlight: Flight = {
+      ...flight,
+      id: this.flights.length + 1,
+    };
+    this.flights = [...this.flights, newFlight];
+    return newFlight;
+  }
+
+  public deleteFlight(id: number): boolean {
+    const length = this.flights.length;
+    this.flights = this.flights.filter((flight) => flight.id !== +id);
+    return length !== this.flights.length;
+  }
+}
