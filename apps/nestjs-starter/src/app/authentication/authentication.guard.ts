abaa5a41d0d4ad33fcf77c20d2b287bb3b94d93f
Marco <m.haemmerle@nemocontra.de>
2019-02-23 13:56:36 +0100

    final pipe labs

---
+++ apps/nestjs-starter/src/app/authentication/authentication.guard.ts
@@ -0,0 +1,35 @@
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { Request } from 'express';
+
+export interface CustomRequest extends Request {
+  user: {
+    id: number;
+    username: string;
+    firstname: string;
+    lastname: string;
+    role: string;
+  };
+}
+
+@Injectable()
+export class AuthenticationGuard implements CanActivate {
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const request = context.switchToHttp().getRequest<CustomRequest>();
+    const authorizationHeader = request.headers['authorization'];
+    if (
+      authorizationHeader &&
+      authorizationHeader.indexOf('Bearer jwt123456token') !== -1
+    ) {
+      return true;
+    }
+    throw new UnauthorizedException();
+  }
+}
