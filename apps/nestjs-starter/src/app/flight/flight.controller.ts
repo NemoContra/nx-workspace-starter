bd026e1415fda9b36c4d25161b7286a7f68d6c0d
Marco <m.haemmerle@nemocontra.de>
2019-02-23 13:56:36 +0100

    final guard labs

---
+++ apps/nestjs-starter/src/app/flight/flight.controller.ts
@@ -0,0 +1,47 @@
+import {
+  Controller,
+  Get,
+  Query,
+  Post,
+  Body,
+  Param,
+  Delete,
+  HttpCode,
+  NotFoundException,
+  HttpStatus,
+  UseGuards,
+} from '@nestjs/common';
+import { Flight } from '@flight-app/shared';
+import { FlightService } from './flight.service';
+import { AuthenticationGuard } from '../authentication/authentication.guard';
+
+@Controller('flight')
+@UseGuards(AuthenticationGuard)
+export class FlightController {
+  constructor(private flightService: FlightService) {}
+
+  @Get()
+  getFlights(@Query('from') from: string, @Query('to') to: string): Flight[] {
+    return this.flightService.getFlights(from, to);
+  }
+
+  @Get(':id')
+  getFlightById(@Param('id') id: string): Flight {
+    return this.flightService.getFlightById(+id);
+  }
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  createFlight(@Body() flight: Flight): Flight {
+    return this.flightService.createFlight(flight);
+  }
+
+  @Delete(':id')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  deleteFlightById(@Param('id') id: string): void {
+    const deleted = this.flightService.deleteFlight(+id);
+    if (!deleted) {
+      throw new NotFoundException('Flight not found.');
+    }
+  }
+}
