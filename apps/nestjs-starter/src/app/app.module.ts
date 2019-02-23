ff4eb7cd97706ce622e9dca2bf7c7a3e3d311db1
Marco <m.haemmerle@nemocontra.de>
2019-02-23 13:56:36 +0100

    final http client labs

--- apps/nestjs-starter/src/app/app.module.ts
+++ apps/nestjs-starter/src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
-
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FlightModule } from './flight/flight.module';
+import { PassengerModule } from './passenger/passenger.module';
+import { CoreModule } from './core/core.module';
 
 @Module({
-  imports: [],
+  imports: [FlightModule, PassengerModule, CoreModule],
   controllers: [AppController],
   providers: [AppService],
 })
