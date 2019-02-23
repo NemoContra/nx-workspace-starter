e9228ecba15b315d31edba08e71e269091b5ee85
Marco <m.haemmerle@nemocontra.de>
2019-02-23 13:56:36 +0100

    final interceptor labs

---
+++ apps/nestjs-starter/src/app/flight/flight.module.ts
@@ -0,0 +1,9 @@
+import { Logger, Module } from '@nestjs/common';
+import { FlightController } from './flight.controller';
+import { FlightService } from './flight.service';
+
+@Module({
+  controllers: [FlightController],
+  providers: [FlightService, Logger, { provide: 'DELAY_TIME', useValue: 2000 }],
+})
+export class FlightModule {}
