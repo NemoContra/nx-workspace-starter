d4f0c3cf40bc5a757bf46ae1e7bd7d4b07ec3091
Marco <m.haemmerle@nemocontra.de>
2019-03-08 13:42:42 +0100

    final typeorm labs

--- apps/nestjs-starter/src/app/flight/flight.service.spec.ts
+++ apps/nestjs-starter/src/app/flight/flight.service.spec.ts
@@ -3,35 +3,39 @@ import { FlightService } from './flight.service';
 import { HttpModule, HttpService } from '@nestjs/common';
 import { of, throwError } from 'rxjs';
 import { Flight } from '@flight-app/shared';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { FlightEntity } from './flight.entity';
+import SpyInstance = jest.SpyInstance;
 
 describe('FlightService', () => {
   let service: FlightService;
   let httpService: HttpService;
+  let mockRepository: MockFlightRepository;
+  let selectQueryBuilderSelectSpy: SpyInstance;
 
   beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [HttpModule],
       providers: [
-        FlightService
+        FlightService,
+        {
+          provide: getRepositoryToken(FlightEntity),
+          useClass: MockFlightRepository
+        }
       ]
     }).compile();
     service = module.get<FlightService>(FlightService);
     httpService = module.get<HttpService>(HttpService);
+    mockRepository = module.get<MockFlightRepository>(getRepositoryToken(FlightEntity));
   });
 
-  it('should return an empty array if nothing is passed in "searchFlights"', () => {
-    expect(service.searchFlights('', '')).toEqual([]);
+  it('should return an empty array if nothing is passed in "searchFlights"', async () => {
+    expect(await service.searchFlights('', '')).toEqual([]);
+    expect(selectQueryBuilderSelectSpy).toHaveBeenCalledWith('flight_entity');
   });
 
-  it('should return correct flights if "Stuttgart" and "Salzburg" is passed in "searchFlights"', () => {
-    const result = service.searchFlights(
-      'Stuttgart',
-      'Salzburg',
-      new Date('2019-02-28T00:06:54'),
-      new Date('2019-03-02T00:08:54')
-    );
-    expect(result.length).toEqual(3);
-    expect(result).toEqual([
+  it('should return correct flights if "Stuttgart" and "Salzburg" is passed in "searchFlights"', async () => {
+    const mockFlights: Flight[] = [
       {
         id: 171,
         from: 'Stuttgart',
@@ -53,7 +57,14 @@ describe('FlightService', () => {
         date: '2019-03-01T11:07:54.1624336+00:00',
         delayed: false
       }
-    ]);
+    ];
+    mockRepository.mockFlights = mockFlights;
+    const result = await service.searchFlights(
+      'Stuttgart',
+      'Salzburg'
+    );
+    expect(result.length).toEqual(3);
+    expect(result).toEqual(mockFlights);
   });
 
   it('should return correct flight for "getFlightById"', () => {
@@ -81,8 +92,8 @@ describe('FlightService', () => {
     expect(httpService.get).toHaveBeenCalledWith('http://www.angular.at/api/flight/4');
   });
 
-  it('should return correct flight for "createFlight"', () => {
-    expect(service.createFlight({
+  it('should return correct flight for "createFlight"', async () => {
+    expect(await service.createFlight({
       from: 'Hamburg',
       to: 'Graz',
       date: '2019-02-22T09:07:54.1624336+00:00',
@@ -96,29 +107,52 @@ describe('FlightService', () => {
     });
   });
 
-  it('should return true for "deleteFlight"', () => {
-    expect(service.deleteFlight(4)).toEqual(true);
+  it('should return true for "deleteFlight"', async () => {
+    mockRepository.deletedFlights = 1;
+    expect(await service.deleteFlight(4)).toEqual(true);
   });
 
-  it('should return false for "deleteFlight"', () => {
-    expect(service.deleteFlight(190)).toEqual(false);
+  it('should return false for "deleteFlight"', async () => {
+    mockRepository.deletedFlights = 0;
+    expect(await service.deleteFlight(190)).toEqual(false);
   });
+
+  class MockFlightRepository {
+    mockFlights = [];
+    deletedFlights = 0;
+
+    createQueryBuilder() {
+      return new MockSelectQueryBuilder(this.mockFlights);
+    }
+
+    save() {
+      return Promise.resolve({
+        id: 174,
+        from: 'Hamburg',
+        to: 'Graz',
+        date: '2019-02-22T09:07:54.1624336+00:00',
+        delayed: false
+      });
+    }
+
+    find() {
+      return Promise.resolve(this.mockFlights);
+    }
+
+    delete() {
+      return Promise.resolve({ affected: this.deletedFlights });
+    }
+  }
+
+  class MockSelectQueryBuilder {
+    constructor(public mockFlights: Flight[]) {
+    }
+
+    select = selectQueryBuilderSelectSpy = jest.fn().mockReturnThis();
+    from = jest.fn().mockReturnThis();
+    where = jest.fn().mockReturnThis();
+    andWhere = jest.fn().mockReturnThis();
+    getMany = jest.fn().mockReturnValue(this.mockFlights);
+  }
 });
 
-class MockFlightRepository {
-  mockFlights = [];
-
-  save() {
-    return Promise.resolve({
-      id: 174,
-      from: 'Hamburg',
-      to: 'Graz',
-      date: '2019-02-22T09:07:54.1624336+00:00',
-      delayed: false
-    });
-  }
-
-  find() {
-    return Promise.resolve(this.mockFlights);
-  }
-}
