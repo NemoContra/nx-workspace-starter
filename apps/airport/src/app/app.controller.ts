58d566bd30ae2b56d0b92ae8361f3c0495a49b99
Marco Hämmerle <m.haemmerle@nemocontra.de>
2021-04-07 21:57:00 +0200

    start microservices labs

--- apps/airport/src/app/app.controller.ts
+++ apps/airport/src/app/app.controller.ts
@@ -1,16 +1,6 @@
-import { CacheInterceptor, CacheKey, Controller, UseInterceptors } from '@nestjs/common';
-import { MessagePattern } from '@nestjs/microservices';
-import { AppService } from './app.service';
-import { Observable } from 'rxjs';
+import { Controller } from '@nestjs/common';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) { }
 
-  @CacheKey('airports')
-  @UseInterceptors(CacheInterceptor)
-  @MessagePattern({cmd: 'airports'})
-  public getAirports(): Observable<string[]> {
-    return this.appService.getAirports();
-  }
 }
--- apps/nestjs-starter/src/app/app.module.ts
+++ apps/nestjs-starter/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FlightModule } from './flight/flight.module';
 import { PassengerModule } from './passenger/passenger.module';
 import { CoreModule } from './core/core.module';
 import { MongooseModule } from '@nestjs/mongoose'
+import { AirportModule } from './airport/airport.module';
 
 @Module({
   imports: [
     FlightModule,
+    AirportModule,
     PassengerModule,
     CoreModule,
     MongooseModule.forRoot('')
