036ef05ebc2ba59450e402419c1d661e3e7e58bc
Florian Tischler <Florian.Tischler@gmx.net>
2023-07-14 10:44:36 +0200

    chore: update ws

--- jest.preset.js
+++ jest.preset.js
@@ -1,4 +1,4 @@
-const nxPreset = require('@nrwl/jest/preset').default;
+const nxPreset = require('@nx/jest/preset').default;
 module.exports = {
   ...nxPreset,
   testMatch: ['**/+(*.)+(spec|test).+(ts|js)?(x)'],
@@ -6,7 +6,7 @@ module.exports = {
   transform: {
     '^.+\\.(ts|js|html)$': 'ts-jest',
   },
-  resolver: '@nrwl/jest/plugins/resolver',
+  resolver: '@nx/jest/plugins/resolver',
   maxConcurrency: 4,
   moduleFileExtensions: ['ts', 'js', 'html'],
   collectCoverage: true,
